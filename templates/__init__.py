d8471d335bc44db98ab28ba286b61a22ae49f897
Milosz Piglas <milosz@archeocs.com>
2020-11-10 20:41:47 +0100

    file templates

---
+++ templates/__init__.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.extend(__path__)
+
+def name():
+    return "as-square"
+
+def description():
+    return "Archeological Survey Squared"
+
+def version():
+    return "$__PLUGIN_VERSION__$"
+
+def qgisMinimumVersion():
+    return "3.10"
+
+def authorName():
+    return u"Milosz Piglas"
+
+def classFactory(iface):
+    import main
+    return main.Plugin(iface)
