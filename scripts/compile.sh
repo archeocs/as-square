09b0cdfe3bc37ae23067c89e421224fbe16a343a
Milosz Piglas <milosz@archeocs.com>
2020-11-13 13:47:42 +0100

    chore: move plugin.xml to src directory
    This file is suposed to be used only as template

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -8,7 +8,7 @@ echo $TAG
 
 sed "s/@__PLUGIN_VERSION__@/$TAG/" src/__init__.py > build/__init__.py
 sed "s/@__PLUGIN_VERSION__@/$TAG/" src/metadata.txt > build/metadata.txt
-sed "s/@__PLUGIN_VERSION__@/$TAG/" plugins.xml > build/plugins.xml
+sed "s/@__PLUGIN_VERSION__@/$TAG/" src/plugins.xml > build/plugins.xml
 
 cp src/test_*.py build/
 cp src/main.py src/layers_manager.py build/
