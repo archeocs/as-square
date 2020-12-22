9017282b0420cbb7b38a601e6d93abf35326bf9d
Milosz Piglas <milosz@archeocs.com>
2020-12-22 10:30:42 +0100

    model definition

--- qtapp/input_tab.py
+++ qtapp/input_tab.py
@@ -4,48 +4,124 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-X_TYPE = QMetaType.User + 100
-
 class Cell:
 
-    def __init__(self, display, key, ctype):
+    def __init__(self, display, key=None):
         self.displayText = display
         self.editText = key
-        if not key:
-            self.editText = display
-        self.ctype = ctype
 
     def __str__(self):
         return 'd={}, k={}'.format(self.displayText, self.editText)
 
-class InputTabWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-        self.setCentralWidget(self.createView())
 
-    def createView(self):
+class Column:
+
+    def __init__(self, allowedValues=None):
+        self.allowed = allowedValues
+        self.combo = allowedValues is not None
+
+    def modelItem(self, val):
+        if self.combo and val in self.allowed:
+            return newItem(Cell(self.allowed[val], val))
+        else:
+            return newItem(Cell(val))
+
+    def getValue(self, item):
+        if self.combo:
+            return item.data(role=Qt.EditRole)
+        else:
+            return item.data(role=Qt.DisplayRole)
+
+class InputTabWidget(QWidget):
+
+    def __init__(self, modelDef, rows, parent=None):
+        QWidget.__init__(self, parent)
+        lay = QVBoxLayout()
+        self.setLayout(lay)
+
+        self.tab = self.initView(modelDef)
+        self.model = self.initModel(modelDef, rows)
+        self.tab.setModel(self.model)
+
+        for (ci, c) in enumerate(modelDef):
+            if c.combo:
+                print(ci, c.allowed)
+                self.tab.setItemDelegateForColumn(ci, mapComboBoxDelegate(c.allowed, self.tab))
+        lay.addWidget(self.tab)
+        lay.addWidget(self.createButtons())
+
+    def initView(self, modelDef):
         tab = QTableView(self)
-        model = self.createModel([[Cell('A', 'a', X_TYPE)]])
-        tab.setModel(model)
-        tab.setItemDelegateForColumn(0, mapComboBoxDelegate({'a': 'A', 'b': 'B'}))
+        modelDef = modelDef
         return tab
 
-    def createModel(self, rows):
-        model = QStandardItemModel(0, len(rows[0]))
-        for r in rows:
-            mrow = list(map(newItem, r))
+    def initModel(self, modelDef, rows):
+        model = QStandardItemModel(0, len(modelDef))
+        for (ri, r) in enumerate(rows):
+            md = modelDef[ri]
+            mrow = list(map(md.modelItem, r))
+            print(mrow)
             model.appendRow(mrow)
         return model
 
+    def addRowAction(self):
+        self.model.appendRow([newItem(Cell('', '?')), newItem(Cell(''))])
+
+    def delRowAction(self):
+        selModel = self.tab.selectionModel()
+        if selModel.hasSelection():
+            idx = selModel.currentIndex()
+            self.model.removeRow(idx.row())
+
+    def createButtons(self):
+        buttons = QGroupBox(self)
+        lay = QHBoxLayout()
+        buttons.setLayout(lay)
+
+        addBtn = QPushButton('+')
+        addBtn.clicked.connect(self.addRowAction)
+
+        delBtn = QPushButton('-')
+        delBtn.clicked.connect(self.delRowAction)
+
+        okBtn = QPushButton('OK')
+        cancelBtn = QPushButton('Cancel')
+
+        lay.addWidget(addBtn)
+        lay.addWidget(delBtn)
+        lay.addWidget(okBtn)
+        lay.addWidget(cancelBtn)
+
+        return buttons
+
+
+class InputTabWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        columns = [
+            Column({'a': 'A', 'b': 'B', '?': ''})
+            ,Column()
+            ,Column()
+            ,Column({'x': 'X', 'y': 'YYYYYY', '?': ''})
+        ]
+        rows = [
+            ['a', '1111', '5555', '?']
+        ]
+        self.setCentralWidget(InputTabWidget(columns, rows, self))
+
 def newItem(value):
-    sit = TypedItem(value.ctype)
+    print(value)
+    if value.editText:
+        sit = TypedItem()
+        sit.setData(value.editText, role=Qt.EditRole)
+    else:
+        sit = QStandardItem()
     sit.setEditable(True)
     sit.setData(value.displayText, role=Qt.DisplayRole)
-    sit.setData(value.editText, role=Qt.EditRole)
     return sit
 
-def mapComboBoxDelegate(options):
-    delegate = MapComboBoxDelegate(options) 
+def mapComboBoxDelegate(options, parent):
+    delegate = MapComboBoxDelegate(options, parent) 
     return delegate
 
 class MapComboBoxDelegate(QStyledItemDelegate):
@@ -75,7 +151,6 @@ class MapComboBox(QComboBox):
         QComboBox.__init__(self, parent)
 
     def setKey(self, k):
-        print('set key ', k)
         self.setCurrentIndex(self.findData(k))
 
     def key(self):
@@ -83,16 +158,11 @@ class MapComboBox(QComboBox):
 
 class TypedItem(QStandardItem):
 
-    def __init__(self, t):
+    def __init__(self):
         QStandardItem.__init__(self)
-        self._type = t
         self.variants = {}
 
-    def type(self):
-        return self._type
-
     def setData(self, v, role=Qt.UserRole + 1):
-        print('set data', v, role)
         self.variants[role] = v
         self.emitDataChanged()
 
