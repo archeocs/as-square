eace2295482343ec7c4d20f8cc47a718aa789321
Milosz Piglas <milosz@archeocs.com>
2021-01-08 17:06:13 +0100

    parse migration script

--- src/migration.py
+++ src/migration.py
@@ -15,19 +15,50 @@ def selectQuery(db, query, params={}):
         for x in range(fc):
             field = rec.field(x)
             row[field.name()] = field.value()
-            rows.append(row)
-            print('row', row)
+        rows.append(row)
     print('finish')
     return rows, q.lastError()
 
-def checkVersion(dbPath):
+def checkVersion(dbPath, last):
     db = QSqlDatabase.addDatabase('QSPATIALITE')
     db.setDatabaseName(dbPath)
     db.open()
 
     dbver = selectQuery(db,
-                        'SELECT * FROM as_settings WHERE skey = :k',
-                        {'k': 'DB_VERSION'})
-    dv2 = selectQuery(db, 'select * from as_settings')
+                        "SELECT * FROM as_settings WHERE SKEY='DB_VERSION'")
     db.close()
-    return dbver, dv2
+    print(dbver)
+    if dbver[0]:
+        row = dbver[0][0]
+        saved = row.get('SVALUE', None) or row.get('svalue', None)
+        return int(saved) - int(last)
+    else:
+        print(dbver[1].text())
+    return -1
+
+def readScript(scriptPath):
+    with open(scriptPath) as sf:
+        versions = [(), ()]
+        nextVer = []
+        nextStmt = ''
+        for r in sf.readlines():
+            row = r.strip()
+            if row == '':
+                continue
+            print(row)
+            if row.startswith('-- ver:'):
+                versions.append(nextVer)
+                nextVer = []
+            elif row.endswith(';'):
+                nextStmt += ' ' + row[:-1]
+                nextVer.append(nextStmt)
+                nextStmt = ''
+            else:
+                nextStmt += ' ' + row
+        print(versions, len(versions))
+        return versions
+
+
+if __name__ == '__main__':
+    script = readScript('/home/milosz/git/as-square/assquare-migration-db.sql')
+    print(checkVersion('/home/milosz/git/as-square/empty-db.sqlite', len(script) - 1))
