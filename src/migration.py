e943cfd8397264674fac4b3820bc4bffbd492890
Milosz Piglas <milosz@archeocs.com>
2021-01-09 16:13:59 +0100

    running migration script

--- src/migration.py
+++ src/migration.py
@@ -19,6 +19,14 @@ def selectQuery(db, query, params={}):
     print('finish')
     return rows, q.lastError()
 
+def updateQuery(db, query, params={}):
+    print('query', query)
+    q = QSqlQuery(db)
+    for (k, v) in params.items():
+        print('bind ', k, v)
+        q.bindValue(':'+k, v)
+    return (q.exec_(query), q.lastError())
+
 def checkVersion(dbPath, last):
     db = QSqlDatabase.addDatabase('QSPATIALITE')
     db.setDatabaseName(dbPath)
@@ -58,7 +66,31 @@ def readScript(scriptPath):
         print(versions, len(versions))
         return versions
 
+def applyVersion(dbPath, stmts, num):
+    db = QSqlDatabase.addDatabase('QSPATIALITE')
+    db.setDatabaseName(dbPath)
+    db.open()
+    for s in stmts:
+        result = updateQuery(db, s)
+        if not result[0]:
+            print('Migration failure on version', num, result[1].databaseText(), result[1].text())
+            db.rollback()
+            db.close()
+            return
+    ver = updateQuery(db, "UPDATE AS_SETTINGS SET SVALUE='{}' WHERE SKEY='{}'".format(num, 'DB_VERSION'))
+    print('VER ', ver[0], ver[1].databaseText())
+    print(db.commit())
+    print('updated to version', num)
+    db.close()
 
 if __name__ == '__main__':
+    db = '/home/milosz/git/as-square/empty-db.sqlite'
     script = readScript('/home/milosz/git/as-square/assquare-migration-db.sql')
-    print(checkVersion('/home/milosz/git/as-square/empty-db.sqlite', len(script) - 1))
+    verDiff = checkVersion(db, len(script) - 1)
+    start = len(script) - verDiff
+    if verDiff < 0:
+        for (vi, v) in enumerate(script):
+            applyVersion(db, v, vi)
+    else:
+        print('DB up to date')
+    print('OK')
